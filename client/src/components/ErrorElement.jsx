9b0dff08e4391f6c8364824129041363cad91761
rash-123 <vermarashmi559@gmail.com>
2024-02-28 23:32:13 +0530

    redeploy test

--- client/src/components/ErrorElement.jsx
+++ client/src/components/ErrorElement.jsx
@@ -2,7 +2,7 @@ import { useRouteError } from "react-router-dom";
 
 const ErrorElement = () => {
     const error = useRouteError();
-    console.log(error);
+    console.log("error", error);
     return <h4>There was an error....</h4>;
 };
 
